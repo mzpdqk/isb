e07c4e339fb147b788b8439eda9f1c4bc1d5295f
Andrey Khramov <159222638+mzpdqk@users.noreply.github.com>
2025-03-17 21:30:49 +0400

    Update main.py

--- ISB_LAB_1/Part1/main.py
+++ ISB_LAB_1/Part1/main.py
@@ -1,11 +1,18 @@
 import json
 
-def vigenere_encrypt(text, key):
+def vigenere_encrypt(text: str, key: str) -> str:
+    """
+    Шифруем текст шифром Виженера.
+
+    :param text: Исходный текст для шифрования.
+    :param key: Ключ шифрования.
+    :return: Зашифрованный текст.
+    """
     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     encrypted_text = ''
     key = key.lower()
     key_index = 0
-    
+
     for char in text.lower():
         if char in alphabet:
             char_pos = alphabet.index(char)
@@ -18,14 +25,38 @@ def vigenere_encrypt(text, key):
             encrypted_text += char
     return encrypted_text
 
-with open('old_text.txt', 'r', encoding='utf-8') as f:
-    original_text = f.read()
+def read_text_from_file(file_path: str) -> str:
+    """
+    Текст из файла.
 
-with open('key.json', 'r', encoding='utf-8') as f:
-    key = json.load(f)['key']
+    :param file_path: Путь к файлу.
+    :return: Текст файла.
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.read()
 
+def read_key_from_json(file_path: str) -> str:
+    """
+    Ключ из JSON.
+
+    :param file_path: Путь к JSON файлу.
+    :return: Ключ шифрования.
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return json.load(f)['key']
+
+def write_text_to_file(file_path: str, text: str) -> None:
+    """
+    Зашифрованный текст.
+
+    :param file_path: Путь к файлу.
+    :param text: Зашифрованный текст.
+    """
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(text)
+        
+original_text = read_text_from_file('old_text.txt')
+key = read_key_from_json('key.json')
 encrypted_text = vigenere_encrypt(original_text, key)
-
-with open('new_text.txt', 'w', encoding='utf-8') as f:
-    f.write(encrypted_text)
+write_text_to_file('new_text.txt', encrypted_text)
 print("Готово. Зашифрованный текст в new_text.")
